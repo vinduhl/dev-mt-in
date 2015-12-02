65629cad88a26428d6a48fb6ec27c138feec6b01
Ryan Walsh <ryan@thehatrack.net>
2015-12-02 13:57:17 -0700

    Initial commit

--- homeCtrl.js
+++
@@ -1,8 +0,0 @@
-angular.module('devMtIn')
-.controller('homeCtrl', function( $scope ) {
-
-	$scope.myProfile = {
-		friends: [{name: 'Ryan'}, {name: 'Bryan'}, {name: 'Sarah'}, {name: 'Zac'}, {name: 'Erin'}]
-	}
-
-});
